7f575bba0753a1c5451fdab9cecb8560867bf4b2
Bob Delamater <bob.delamater@sage.com>
2015-07-07 14:50:57 -0700

    Update WipFinalizationActivityCodeCapacityCheck.sql

--- Case/8004376158/WipFinalizationActivityCodeCapacityCheck.sql
+++ Case/8004376158/WipFinalizationActivityCodeCapacityCheck.sql
@@ -21,6 +21,10 @@ DECLARE @NMM_MAX	INT,
 		@NMIO_FormulaResult	INT		-- No. OH lines Products
 
 /**************************** Variable Assignment ****************************/
+SET @ONA_MAX =	(SELECT DIME_0 FROM ECCPROD2.ACTIV WHERE CODACT_0 = 'ONA')
+SET @MAT_MAX =	(SELECT DIME_0 FROM ECCPROD2.ACTIV WHERE CODACT_0 = 'MAT')
+SET @LAB_MAX =	(SELECT DIME_0 FROM ECCPROD2.ACTIV WHERE CODACT_0 = 'LAB')
+SET @MAC_MAX =	(SELECT DIME_0 FROM ECCPROD2.ACTIV WHERE CODACT_0 = 'MAC')
 
 SET @NMM_MAX=	(SELECT DIMMAX_0 FROM ECCPROD2.ACTIV WHERE CODACT_0 = 'NMM')
 SET @NMMF_MAX = (SELECT DIMMAX_0 FROM ECCPROD2.ACTIV WHERE CODACT_0 = 'NMMF') -- NMM*max(1,MAT,MAC,LAB)
@@ -31,12 +35,7 @@ SET @NMIF_MAX = (SELECT DIMMAX_0 FROM ECCPROD2.ACTIV WHERE CODACT_0 = 'NMIF') --
 SET @NMIO_MAX = (SELECT DIMMAX_0 FROM ECCPROD2.ACTIV WHERE CODACT_0 = 'NMIO') -- NMI*ONA
 
 SET @NMW_MAX  =	(SELECT DIMMAX_0 FROM ECCPROD2.ACTIV WHERE CODACT_0 = 'NMW')  
-SET @NMMO_MAX = (SELECT @NMW_MAX * @ONA_MAX)
-
-SET @ONA_MAX =	(SELECT DIME_0 FROM ECCPROD2.ACTIV WHERE CODACT_0 = 'ONA')
-SET @MAT_MAX =	(SELECT DIME_0 FROM ECCPROD2.ACTIV WHERE CODACT_0 = 'MAT')
-SET @LAB_MAX =	(SELECT DIME_0 FROM ECCPROD2.ACTIV WHERE CODACT_0 = 'LAB')
-SET @MAC_MAX =	(SELECT DIME_0 FROM ECCPROD2.ACTIV WHERE CODACT_0 = 'MAC')
+SET @NMWO_MAX = (SELECT @NMW_MAX * @ONA_MAX)
 
 SET @NMMF_FormulaResult = 
 (	
@@ -80,94 +79,202 @@ IF OBJECT_ID('tempdb..#MaxListings', 'U') IS NOT NULL
 BEGIN
 	DROP TABLE #MaxListings
 END
---SELECT 
---	@NMM_MAX NMM_MAX,
---	@NMMF_MAX NMMF_MAX, 
---	@NMMO_MAX NMMO_MAX, 
---	@NMI_MAX NMI_MAX, 
---	@NMIF_MAX NMIF_MAX, 
---	@NMIO_MAX NNIO_MAX, 
---	@ONA_MAX ONA_MAX, 
---	@MAT_MAX MAT_MAX, 
---	@LAB_MAX LAB_MAX, 
---	@MAC_MAX MAC_MAX,
---	@NMMF_FormulaResult NMMF_FormulaResult,
---	@NMMO_FormulaResult NMMO_FormulaResult,
---	@NMIF_FormulaResult NMIF_FormulaResult,
---	@NMIO_FormulaResult NMIO_FormulaResult
---INTO #MaxListings
 CREATE TABLE #MaxListings
 (	
 	ID					INT	PRIMARY KEY IDENTITY(1,1),
 	ActivityCode		VARCHAR(MAX) NOT NULL,
 	ActivityCodeValue	INT
 )
-INSERT INTO #MaxListings (ActivityCode, ActivityCodeValue)
 
-SELECT 'NMM', @NMM_MAX								-- No. material lines					Table: MFGMATRK -- easy
-UNION
-SELECT 'NMI', @NMI_MAX								-- No. product lines					Table: MFGITMTRK --  easy
-UNION
-SELECT 'NMMF', @NMMF_MAX							-- Number of lines groups				Screen: CLCCST block 4 only How do I know what my min value can be?
-UNION
-SELECT 'NMMO', @NMMO_MAX							-- No. OH lines Materials				Screen: CLCCST How do I know what my min value can be?
-UNION
-SELECT 'NMW', @NMW_MAX								-- No. operation lines					Table: MFGOPETRK -- easy
-UNION
-SELECT 'NMWO', @NMWO_MAX							-- No. OH lines Operations				How do I know what my minimum value can be?
-UNION
-SELECT 'ONA', @ONA_MAX								-- Number of general cost natures		How do I know what my minimum value can be? GESONA (count the number of records)
-UNION
-SELECT 'MAT', @MAT_MAX								-- No. material groups					Screen: ITM2 BRDCOD (Costing Family) -- Local 325 compare it with ITMMASTER BRDCOD field usage
-UNION
-SELECT 'LAB', @LAB_MAX								-- No. labour groups					Screen: MWC1 BRDCOD (Cost Group) ---- NOTE: Check local menu 316 against WORKCOST to find which cost family is not in use for labor type
-UNION
-SELECT 'MAC', @MAC_MAX								-- No. machine groups					Screen: MWC1 BRDCOD (Cost Group) ---- NOTE: Check local menu 316 against WORKCOST to find which cost family is not in use for machine type
-UNION
-SELECT 'NMMF_FormulaResult', @NMMF_FormulaResult	
-UNION
-SELECT 'NMMO_FormulaResult', @NMMO_FormulaResult	
-UNION
-SELECT 'NMIF_FormulaResult', @NMIF_FormulaResult	
-UNION
-SELECT 'NMIO_FormulaResult', @NMIO_FormulaResult	
+INSERT INTO #MaxListings (ActivityCode, ActivityCodeValue)
+VALUES('NMM', @NMM_MAX)	
+
+INSERT INTO #MaxListings (ActivityCode, ActivityCodeValue)
+VALUES('NMI', @NMI_MAX)	
+
+INSERT INTO #MaxListings (ActivityCode, ActivityCodeValue)
+VALUES('NMMF', @NMMF_MAX)	
+
+INSERT INTO #MaxListings (ActivityCode, ActivityCodeValue)
+VALUES('NMMO', @NMMO_MAX)	
+
+INSERT INTO #MaxListings (ActivityCode, ActivityCodeValue)
+VALUES('NMW', @NMW_MAX)	
+
+INSERT INTO #MaxListings (ActivityCode, ActivityCodeValue)
+VALUES('NMWO', @NMWO_MAX)	
+
+INSERT INTO #MaxListings (ActivityCode, ActivityCodeValue)
+VALUES('ONA', @ONA_MAX)	
+
+INSERT INTO #MaxListings (ActivityCode, ActivityCodeValue)
+VALUES('MAT', @MAT_MAX)	
+
+INSERT INTO #MaxListings (ActivityCode, ActivityCodeValue)
+VALUES('LAB', @LAB_MAX)	
+
+INSERT INTO #MaxListings (ActivityCode, ActivityCodeValue)
+VALUES('MAC', @MAC_MAX)	
+
+
+/* Formula Results */
+INSERT INTO #MaxListings (ActivityCode, ActivityCodeValue)
+VALUES('NMMF_FormulaResult', @NMMF_FormulaResult	)	
+
+INSERT INTO #MaxListings (ActivityCode, ActivityCodeValue)
+VALUES('NMMO_FormulaResult', @NMMO_FormulaResult)	
+
+INSERT INTO #MaxListings (ActivityCode, ActivityCodeValue)
+VALUES('NMIF_FormulaResult', @NMIF_FormulaResult)	
+
+INSERT INTO #MaxListings (ActivityCode, ActivityCodeValue)
+VALUES('NMIO_FormulaResult', @NMIO_FormulaResult)	
+
+
 
 /********************************* Output ************************************/
-SELECT * FROM #MaxListings
+SELECT ActivityCode, ActivityCodeValue, 
+	'IsInvalid' = 
+		CASE WHEN ActivityCodeValue > 32000 THEN 'True'
+		ELSE 'False'
+		END
+FROM #MaxListings
+ORDER BY IsInvalid DESC
+
+
+/**************** NMM_MAX check ****************/
+-- No. material lines					
+--Table: MFGMATRK 
+DECLARE @NumMatTrackingsCannotBeCosted INT
+SET @NumMatTrackingsCannotBeCosted = 
+(
+	SELECT COUNT(*)
+	FROM ECCPROD2.MFGMATTRK mt
+	GROUP BY mt.MFGNUM_0
+	HAVING COUNT(MFGTRKNUM_0) > @NMM_MAX
+)
+
+IF @NumMatTrackingsCannotBeCosted > 0
+BEGIN
+	SELECT 
+		MFGNUM_0, 
+		COUNT(MFGTRKNUM_0) CntMfgTrkNum, 
+		'These Work Orders cannot be costed' CannotBeCosted
+	FROM ECCPROD2.MFGMATTRK mt
+	GROUP BY mt.MFGNUM_0
+	HAVING COUNT(MFGTRKNUM_0) > @NMM_MAX
+	ORDER BY COUNT(MFGTRKNUM_0) DESC	
+END
+
+
+/**************** NMI_MAX check ****************/
+-- No. product lines					
+--Table: MFGITMTRK 
+DECLARE @NumItmTrackingsCannotBeCosted INT
+SET @NumItmTrackingsCannotBeCosted = 
+(
+	SELECT COUNT(*)
+	FROM ECCPROD2.MFGITMTRK it
+	GROUP BY it.MFGNUM_0
+	HAVING COUNT(MFGTRKNUM_0) > @NMI_MAX
+)
+
+
+IF @NumItmTrackingsCannotBeCosted > 0
+BEGIN
+	SELECT 
+		MFGNUM_0, 
+		COUNT(MFGTRKNUM_0) CntMfgTrkNum, 
+		'These Work Orders cannot be costed' CannotBeCosted
+	FROM ECCPROD2.MFGITMTRK it
+	GROUP BY it.MFGNUM_0
+	HAVING COUNT(MFGTRKNUM_0) > @NMI_MAX
+	ORDER BY COUNT(MFGTRKNUM_0) DESC
+END
+
+/**************** NMW_MAX check ****************/
+-- No. operation lines					
+-- Table: MFGOPETRK 
+DECLARE @NumOpeTrackingsCannotBeCosted INT
+SET @NumOpeTrackingsCannotBeCosted= 
+(
+	SELECT COUNT(*)
+	FROM ECCPROD2.MFGOPETRK ot
+	GROUP BY ot.MFGNUM_0
+	HAVING COUNT(ot.MFGTRKNUM_0) > @NMI_MAX
+)
+
+
+IF @NumOpeTrackingsCannotBeCosted > 0
+BEGIN
+	SELECT 
+		ot.MFGNUM_0, 
+		COUNT(ot.MFGTRKNUM_0) CntMfgTrkNum, 
+		'These Work Orders cannot be costed' CannotBeCosted
+	FROM ECCPROD2.MFGOPETRK ot
+	GROUP BY ot.MFGNUM_0
+	HAVING COUNT(ot.MFGTRKNUM_0) > @NMI_MAX
+	ORDER BY COUNT(ot.MFGTRKNUM_0) DESC
+END
+
+/**************** NMMF_MAX check ****************/
+-- Number of lines groups				
+-- Screen: CLCCST block 4 only How do I know what my min value can be?
+
+/**************** NMMO_MAX check ****************/
+-- No. OH lines Materials				
+-- Screen: CLCCST How do I know what my min value can be?
+
+/**************** NMWO_MAX check ****************/
+-- No. OH lines Operations				
+-- How do I know what my minimum value can be?
+
+/**************** ONA_MAX check ****************/
+-- Number of general cost natures		
+-- How do I know what my minimum value can be? GESONA (count the number of records)
+IF @ONA_MAX > (SELECT COUNT(*) FROM ECCPROD2.OVENAT)
+BEGIN
+	SELECT 
+		@ONA_MAX CurrentOnaMax, 
+		'You can reduce activity code ONA to ' 
+		+ CAST((SELECT COUNT(*) FROM ECCPROD2.OVENAT) AS VARCHAR(MAX)) ProposedNewOnaMax
+END
+
+/**************** MAT_MAX check ****************/
+-- No. material groups					
+-- Screen: ITM2 BRDCOD (Costing Family) -- Local 325 compare it with ITMMASTER BRDCOD field usage
+SELECT *
+FROM ECCPROD2.APLSTD
+WHERE 
+	LAN_0 = 'ENG'
+	AND LANCHP_0 = 325
+
+/**************** LAB_MAX check ****************/
+-- No. labour groups					
+-- Screen: MWC1 BRDCOD (Cost Group) ---- NOTE: Check local menu 316 against WORKCOST to find which cost family is not in use for labor type
+
+/**************** MAC_MAX check ****************/
+-- No. machine groups					
+-- Screen: MWC1 BRDCOD (Cost Group) ---- NOTE: Check local menu 316 against WORKCOST to find which cost family is not in use for machine type
 
 
 
-SELECT TOP 100
-	MFGNUM_0, 
-	COUNT(MFGTRKNUM_0) CntMfgTrkNum, 
-	'These Work Orders cannot be costed' CannotBeCosted
-FROM ECCPROD2.MFGMATTRK mt
-GROUP BY mt.MFGNUM_0
---HAVING COUNT(MFGTRKNUM_0) > @NMM_MAX
-ORDER BY COUNT(MFGTRKNUM_0) DESC
-
-SELECT TOP 1
-	MFGNUM_0, 
-	COUNT(MFGTRKNUM_0) CntMfgTrkNum, 
-	'These Work Orders cannot be costed' CannotBeCosted
-FROM ECCPROD2.MFGITMTRK it
-GROUP BY it.MFGNUM_0
---HAVING COUNT(MFGTRKNUM_0) > @NMI_MAX
-ORDER BY COUNT(MFGTRKNUM_0) DESC
 
 
-select VLTCCE_0, BRDCOD_0
-from ECCPROD2.WORKCOST
 
 
-select *
-from ECCPROD2.APLSTD
-
-select * from ECCPROD2.ACTIV where ACTFOR_0 <> ''
+--select VLTCCE_0, BRDCOD_0
+--from ECCPROD2.WORKCOST
 
 
-select TCLCOD_0, VLTCOD_0, STOMGTCOD_0  from ECCPROD2.ITMCATEG where VLTCOD_0 <> 'STD'
+--select *
+--from ECCPROD2.APLSTD
 
-select ITMREF_0, STOFCY_0, VLTCOD_0
-from ECCPROD2.ITMFACILIT
-where VLTCOD_0 <> 'STD'
+--select * from ECCPROD2.ACTIV where ACTFOR_0 <> ''
+
+
+--select TCLCOD_0, VLTCOD_0, STOMGTCOD_0  from ECCPROD2.ITMCATEG where VLTCOD_0 <> 'STD'
+
+--select ITMREF_0, STOFCY_0, VLTCOD_0
+--from ECCPROD2.ITMFACILIT
+--where VLTCOD_0 <> 'STD'
