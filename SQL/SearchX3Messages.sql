4643161c0262eb1b53a40bc8d62f9de426358a64
Bob Delamater <bob.delamater@sage.com>
2015-09-01 14:11:24 -0700

    Fix case comparison
    Now you can search messages in in upper and lower case

--- SQL/SearchX3Messages.sql
+++ SQL/SearchX3Messages.sql
@@ -7,5 +7,5 @@ SELECT
 	'mess(' + CONVERT(VARCHAR(10), LANNUM_0) + ',' + CONVERT(VARCHAR(10), LANCHP_0) + ',1' + ')' PlugMeIntoTheX3Calculator, 
 	LANMES_0, LAN_0 
 FROM <Folder Name,VARCHAR(255),SUPV5>.APLSTD 
-WHERE LOWER(LANMES_0) like N'%<Search Value typed in lower case,VARCHAR(255),refused access%>%'
+WHERE LOWER(LANMES_0) like LOWER(N'%<Search Value typed in lower case,VARCHAR(255),refused access%>%')
 AND LAN_0 = N'<Language,VARCHAR(25),ENG>'
